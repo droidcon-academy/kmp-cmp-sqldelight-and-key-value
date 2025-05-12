31f187ef288f3bf410384b2a63442dee05488279
mohan.sandeep <mohan.sandeep@phonepe.com>
2025-05-12 09:48:07 +0530

    Added for ios

--- source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/MainViewController.kt
+++ source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/MainViewController.kt
@@ -3,17 +3,19 @@ package com.droidcon.habitsync
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.droidcon.habitsync.db.createDatabaseHelper
+import com.droidcon.habitsync.repository.HabitRepository
+import com.droidcon.habitsync.ui.home.MainHabitUI
+import com.droidcon.habitsync.viewmodel.HabitViewModel
 
 //fun MainViewController() = ComposeUIViewController {
 //  //  App(createDataStore())
 //}
 
 fun MainViewController() = ComposeUIViewController {
-    val dbHelper = remember {
-        DatabaseHelper(DriverFactory().createDriver())
-    }
+    val dbHelper = createDatabaseHelper()
+    val repository = HabitRepository(dbHelper)
+    val viewModel = HabitViewModel(repository)
 
-    MaterialTheme {
-        UserScreen(dbHelper = dbHelper)
-    }
+    MainHabitUI(viewModel = viewModel)
 }
--- source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/db/IosDatabaseHelper.kt
+++ source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/db/IosDatabaseHelper.kt
@@ -2,6 +2,8 @@ package com.droidcon.habitsync.db
 
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 
+
+
 fun createDatabaseHelper(): DatabaseHelper {
     val driver = NativeSqliteDriver(HabitDatabase.Schema, "habit.db")
     return object : DatabaseHelper {
