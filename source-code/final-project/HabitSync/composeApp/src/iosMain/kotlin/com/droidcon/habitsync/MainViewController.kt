9b7a510cd08935831e4da241cf8f459764496cae
mohan.sandeep <mohan.sandeep@phonepe.com>
2025-05-12 17:54:15 +0530

    Added ios functionality

--- source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/MainViewController.kt
+++ source-code/final-project/HabitSync/composeApp/src/iosMain/kotlin/com/droidcon/habitsync/MainViewController.kt
@@ -1,30 +1,33 @@
 package com.droidcon.habitsync
 
-import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.droidcon.habitsync.db.createDatabaseHelper
-import com.droidcon.habitsync.db.createIosSqlDriver
 import com.droidcon.habitsync.repository.HabitLogRepository
 import com.droidcon.habitsync.repository.HabitRepository
 import com.droidcon.habitsync.ui.home.MainHabitUI
+import com.droidcon.habitsync.ui.settings.ThemeManager
+import com.droidcon.habitsync.ui.theme.AppTheme
 import com.droidcon.habitsync.viewmodel.HabitViewModel
 
-//fun MainViewController() = ComposeUIViewController {
-//  //  App(createDataStore())
-//}
-
 fun MainViewController() = ComposeUIViewController {
-    val dbHelper = createDatabaseHelper()
-    val logRepo = HabitLogRepository(dbHelper)
+    val prefs = remember { createDataStore() }  // iOS-specific DataStore
+    val themeManager = remember { ThemeManager(prefs) }
+    val dbHelper = remember { createDatabaseHelper() }
 
-    val habitRepository = HabitRepository(dbHelper)
-    val habitLogRepository = HabitLogRepository(dbHelper)
-    val habitViewModel = HabitViewModel(habitRepository, habitLogRepository)
-
-    MainHabitUI(
-        habitViewModel = habitViewModel,
-        logRepo = logRepo,
-        dbHelper = dbHelper
-    )
+    val habitRepository = remember { HabitRepository(dbHelper) }
+    val habitLogRepository = remember { HabitLogRepository(dbHelper) }
+    val habitViewModel = remember { HabitViewModel(habitRepository, habitLogRepository) }
+    AppTheme(themeManager) {
+        MainHabitUI(
+            habitViewModel = habitViewModel,
+            logRepo = habitLogRepository,
+            dbHelper = dbHelper,
+            themeManager = themeManager
+        )
+    }
 }
+
